9912209ba223013339c8cd18911ff2c286cbc5a7
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-09-24 21:10:26 +0000

    Automated update

--- src/Soenneker.Render.OpenApiClient/Models/ServiceEventWithCursor.cs
+++ src/Soenneker.Render.OpenApiClient/Models/ServiceEventWithCursor.cs
@@ -15,6 +15,14 @@ namespace Soenneker.Render.OpenApiClient.Models
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
+        /// <summary>The cursor property</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public string? Cursor { get; set; }
+#nullable restore
+#else
+        public string Cursor { get; set; }
+#endif
         /// <summary>The event property</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
@@ -48,6 +56,7 @@ namespace Soenneker.Render.OpenApiClient.Models
         {
             return new Dictionary<string, Action<IParseNode>>
             {
+                { "cursor", n => { Cursor = n.GetStringValue(); } },
                 { "event", n => { Event = n.GetObjectValue<global::Soenneker.Render.OpenApiClient.Models.ServiceEventWithCursorEvent>(global::Soenneker.Render.OpenApiClient.Models.ServiceEventWithCursorEvent.CreateFromDiscriminatorValue); } },
             };
         }
@@ -58,6 +67,7 @@ namespace Soenneker.Render.OpenApiClient.Models
         public virtual void Serialize(ISerializationWriter writer)
         {
             if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
+            writer.WriteStringValue("cursor", Cursor);
             writer.WriteObjectValue<global::Soenneker.Render.OpenApiClient.Models.ServiceEventWithCursorEvent>("event", Event);
             writer.WriteAdditionalData(AdditionalData);
         }
